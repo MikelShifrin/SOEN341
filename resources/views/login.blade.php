419e8e017e3059a54335ec0d8b3b96c278077a88
vivekvharibal <vivekvhari@gmail.com>
2017-10-13 21:40:19 -0400

    Login Page
    Creation of Login Page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Signin</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="{{URL::to('css/bootstrap.min.css')}}">
+
+    <!-- Custom styles for this template -->
+    <link href="{{URL::to('css/signin.css')}}" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div class="container">
+
+    <form class="form-signin" action="/login" method="post">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+        </div>
+        {{csrf_field()}}
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
+
+</div> <!-- /container -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+{{--<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>--}}
+<script src="js/jquery-3.2.1.slim.min.js" ></script>
+<script src="js/tether.min.js" ></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
