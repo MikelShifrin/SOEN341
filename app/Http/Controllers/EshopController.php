4bfb1a5ad19b7ffaf54ffde8364ce8730045b0fc
vivekvharibal <vivekvhari@gmail.com>
2017-10-13 21:16:58 -0400

    Creation of controller class: EshopController
    Creation of controller class: EshopController

---
+++ app/Http/Controllers/EshopController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vivek
+ * Date: 13-10-2017
+ * Time: 19:15
+ */
+
+namespace App\Http\Controllers;
+
+
+class EshopController extends Controller
+{
+
+}
