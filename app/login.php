53426f232f72afed8183b2e6f14956619ddd1aac
gagan <singhgagan008@gmail.com>
2017-10-14 06:06:56 -0400

    delete the wrong login model which was just for testing

--- app/login.php
+++
@@ -1,9 +0,0 @@
-namespace App
-
-
-    public function editPost($session, $id, $title, $content)
-    {
-        $posts = $session->get('posts');
-        $posts[$id] = ['title' => $title, 'content' => $content];
-        $session->put('posts', $posts);
-    }
