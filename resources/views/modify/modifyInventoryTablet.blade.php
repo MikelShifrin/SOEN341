c9cfbc8a30ecbff3205f9fd59e024edb2d75f581
eglen <eglen@live.com>
2017-11-08 20:12:33 -0500

    Modify items

--- resources/views/modify/modifyInventoryTablet.blade.php
+++ resources/views/modify/modifyInventoryTablet.blade.php
@@ -190,7 +190,7 @@
                 document.getElementById("m3").value = rowIdArray['Price'];
                 document.getElementById("m4").value = rowIdArray['Processor Type'];
                 document.getElementById("m5").value = rowIdArray['Ram Size'];
-                document.getElementById("m6").value = rowIdArray['CPU Cores'];
+                document.getElementById("m6").value = rowIdArray['Cpu Cores'];
                 document.getElementById("m7").value = rowIdArray['Hard Disk Size'];
                 document.getElementById("m8").value = rowIdArray['Operating System'];
                 document.getElementById("m9").value = rowIdArray['Display Size'];
